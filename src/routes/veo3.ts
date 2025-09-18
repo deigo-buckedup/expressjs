4a80acee0064bbd90f9efdc1b531db77a33b0b89
deigo-lps <diego.lopes0909@gmail.com>
2025-09-18 14:21:47 -0300

    ads try catch

--- src/routes/veo3.ts
+++ src/routes/veo3.ts
@@ -11,42 +11,46 @@ const veo3 = express.Router();
 const upload = multer();
 
 veo3.post('/generate', <any>upload.single('image'), async (req, res) => {
-  const ai = new GoogleGenAI({ apiKey: process.env.apiKey });
+  try {
+    const ai = new GoogleGenAI({ apiKey: process.env.apiKey });
 
-  const generateVideosReq: any = { prompt: req.body.prompt, config: {} };
-  if (req.file) {
-    const imageBytes = req.file.buffer.toString('base64');
-    generateVideosReq.image = {
-      imageBytes,
-      mimeType: req.file.mimetype
-    };
-  }
-  
-  ['aspectRatio', 'resolution', 'negativePrompt'].forEach((key) => {
-    if (req.body[key]) generateVideosReq.config[key] = req.body[key];
-  });
+    const generateVideosReq: any = { prompt: req.body.prompt, config: {} };
+    if (req.file) {
+      const imageBytes = req.file.buffer.toString('base64');
+      generateVideosReq.image = {
+        imageBytes,
+        mimeType: req.file.mimetype
+      };
+    }
 
-  console.log(`Request: ${JSON.stringify(generateVideosReq)}`)
-
-  let operation = await ai.models.generateVideos({
-    model: 'veo-3.0-generate-001',
-    ...generateVideosReq
-  });
-
-  while (!operation.done) {
-    console.log('Waiting for video generation to complete...');
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-    operation = await ai.operations.getVideosOperation({
-      operation: operation
+    ['aspectRatio', 'resolution', 'negativePrompt'].forEach((key) => {
+      if (req.body[key]) generateVideosReq.config[key] = req.body[key];
     });
-  }
 
-  const response = await fetch(
-    operation.response.generatedVideos[0].video?.uri,
-    { headers: { 'x-goog-api-key': process.env.apiKey } }
-  );
-  const arrayBuffer = await response.arrayBuffer();
-  res.type('mp4').send(Buffer.from(arrayBuffer));
+    console.log(`Request: ${JSON.stringify(generateVideosReq)}`);
+
+    let operation = await ai.models.generateVideos({
+      model: 'veo-3.0-generate-001',
+      ...generateVideosReq
+    });
+
+    while (!operation.done) {
+      console.log('Waiting for video generation to complete...');
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+      operation = await ai.operations.getVideosOperation({
+        operation: operation
+      });
+    }
+
+    const response = await fetch(
+      operation.response.generatedVideos[0].video?.uri,
+      { headers: { 'x-goog-api-key': process.env.apiKey } }
+    );
+    const arrayBuffer = await response.arrayBuffer();
+    res.type('mp4').send(Buffer.from(arrayBuffer));
+  } catch (e: any) {
+    res.status(500).json({ error: 'generation_failed', msg: e.message });
+  }
 });
 
 export default veo3;
